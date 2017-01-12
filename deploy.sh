9a803388dbee5a5a28993b02cdded66b93343f22
kumagi <hiroki.kumazaki@gmail.com>
2017-01-12 11:11:52 +0900

    build kicked only after master branch merged

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-# set -o errexit
+set -o errexit
+
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/XXXXX)"
+openssl aes-256-cbc \
+    -K $encrypted_30975c018d00_key \
+    -iv $encrypted_30975c018d00_iv \
+    -in ".travis/github_deploy_key.enc" \
+    -out "$SSH_FILE" -d
+
+chmod 600 "$SSH_FILE" \
+    && printf "%s\n" \
+    "Host github.com" \
+    "  IdentityFile $SSH_FILE" \
+    "  LogLevel ERROR" >> ~/.ssh/config
 
 # config
 git config --global user.email "nobody@nobody.org"
